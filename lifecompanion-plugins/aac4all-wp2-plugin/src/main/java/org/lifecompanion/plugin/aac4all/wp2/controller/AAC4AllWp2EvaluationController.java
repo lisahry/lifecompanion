8d90806d12491c8d6ae17f343fd241548e52b445
lisahry <117349146+lisahry@users.noreply.github.com>
2024-08-05 17:33:49 +0200

    Add StartTrainingAction
    Start Training Action
    Random sequence of training keybords
    Text input exercice of 10 minutes

--- lifecompanion-plugins/aac4all-wp2-plugin/src/main/java/org/lifecompanion/plugin/aac4all/wp2/controller/AAC4AllWp2EvaluationController.java
+++ lifecompanion-plugins/aac4all-wp2-plugin/src/main/java/org/lifecompanion/plugin/aac4all/wp2/controller/AAC4AllWp2EvaluationController.java
@@ -1,25 +1,31 @@
 package org.lifecompanion.plugin.aac4all.wp2.controller;
 
 import javafx.beans.property.BooleanProperty;
+import org.lifecompanion.controller.textcomponent.WritingStateController;
 import org.lifecompanion.model.api.configurationcomponent.LCConfigurationI;
+import org.lifecompanion.model.api.configurationcomponent.SoundResourceHolderI;
 import org.lifecompanion.model.api.lifecycle.ModeListenerI;
 import org.lifecompanion.controller.selectionmode.SelectionModeController;
 import org.lifecompanion.model.api.configurationcomponent.GridComponentI;
 import org.lifecompanion.model.api.configurationcomponent.GridPartComponentI;
-import java.util.Comparator;
-import java.util.Random;
+import org.lifecompanion.model.api.textcomponent.WritingEventSource;
+
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.List;
 
 public enum AAC4AllWp2EvaluationController implements ModeListenerI {
     INSTANCE;
 
     private BooleanProperty evaluationRunning;
+    private List<GridPartComponentI> keyboards;
+    private GridPartComponentI firstEvalKeyboard;
+
 
     AAC4AllWp2EvaluationController() {
     }
 
     private LCConfigurationI configuration;
+
 
     @Override
     public void modeStart(LCConfigurationI configuration) {
@@ -32,15 +38,69 @@ public enum AAC4AllWp2EvaluationController implements ModeListenerI {
     }
 
     public void startDailyEvaluation() {
-        // TODO : démarrer chrono etc.
-
-        List<GridPartComponentI> grids = this.configuration.getAllComponent().values().stream()
+        keyboards = this.configuration.getAllComponent().values().stream()
                 .filter(d -> d instanceof GridPartComponentI)
-                .filter(c -> c.nameProperty().get().startsWith("Clavier"))
+                .filter(c -> c.nameProperty().get().startsWith("Consignes"))
                 .map(c -> (GridPartComponentI)c)
                 .collect(Collectors.toList());
-        GridPartComponentI evalKeyboard = grids.get(new Random().nextInt(grids.size()));
+
+        if (!keyboards.isEmpty()){
+        int indexFirstTrainingKeyboard = new Random().nextInt(keyboards.size());
+        GridPartComponentI evalKeyboard = keyboards.get(indexFirstTrainingKeyboard);
         SelectionModeController.INSTANCE.goToGridPart(evalKeyboard);
+        keyboards.remove(indexFirstTrainingKeyboard);
+        }
+    }
+
+    public void nextDailyEvaluation() {
+
+        if (!keyboards.isEmpty()){
+            int indexTrainingKeyboard = new Random().nextInt(keyboards.size());
+            GridPartComponentI training = keyboards.get(indexTrainingKeyboard);
+            SelectionModeController.INSTANCE.goToGridPart(training);
+            keyboards.remove(indexTrainingKeyboard);
+        }
+        else {
+            System.out.println("c'est fini ");
+        }
+    }
+
+    public void startEvaluation(){
+        // TODO : lancer les claviers selon l'ordre de l'ID
+        //récupérer l'id de l'utilisateur
+
+
+    }
+    public void startTraining(){
+
+        // TODO: clean l'éditeur
+        WritingStateController.INSTANCE.removeAll(WritingEventSource.USER_ACTIONS);
+
+
+        List<GridPartComponentI> EVA =this.configuration.getAllComponent().values().stream()
+                .filter(d -> d instanceof GridPartComponentI)
+                .filter(c -> c.nameProperty().get().startsWith("EVA"))
+                .map(c -> (GridPartComponentI)c)
+                .collect(Collectors.toList()); // trouver un moyen d'aller à l'EVA plus facielement
+
+        // TODO : chrono 10 mins
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                // aller aux EVA
+                SelectionModeController.INSTANCE.goToGridPart(EVA.get(0));
+                //clean l'éditeur
+                WritingStateController.INSTANCE.removeAll(WritingEventSource.USER_ACTIONS);
+                timer.cancel();
+            }
+        };
+        long delay = 10 * 60 * 1000;
+        timer.schedule(timerTask, delay);
+
+
+        // TODO : démarer le listener log
+
 
     }
 }
